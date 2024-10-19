071f30f159eb9e6a33967a388bce2c27b3b3ffb7
Idarteaga <173113463+LastVizard405@users.noreply.github.com>
2024-10-18 21:43:44 -0500

    advances

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,17 +1,21 @@
-const app = requie('../app');
+const app = require('../app');
 const request = require('supertest');
+
+let token;
+
 const BASE_URL = '/api/v1/users';
+
 const user = {
 	firstName: 'Ivan',
 	lastName: 'Arteaga',
 	email: 'ivan@yahoo.com.ar',
 	password: 'ivan1234',
-	gender: 'male',
+	gender: 'Male',
 };
 
 test("POST -> 'BASE_URL', should responde status code 201, and res.body.email === user.email", async () => {
 	const res = await request(app).post(BASE_URL).send(user);
-
+	console.log(res.body);
 	expect(res.status).toBe(201);
 	expect(res.body).toBeDefined();
 	expect(res.body.id).toBeDefined();
@@ -32,4 +36,37 @@ test("POST -> 'BASE_URL/login', should return statusCode 200,  res.body.user and
 	expect(res.body.token).toBeDefined();
 
 	expect(res.body.user.email).toBe(user.email);
+});
+
+test("POST -> 'BASE_URL/login', should return statusCode 401", async () => {
+	const res = await request(app).post(`${BASE_URL}/login`).send({
+		email: 'email@false.com',
+		password: 'ivan1234',
+	});
+
+	expect(res.status).toBe(401);
+	expect(res.body).toBeDefined();
+	expect(res.body.error).toBe('Error, invalid credentials');
+});
+
+test("Get -> 'BASE_URL', should return statusCode 200, and res.body.lentgth === 1", async () => {
+	const res = await request(app).get(BASE_URL).set('Authorization', `Bearer ${token}`);
+
+	expect(res.status).toBe(200);
+	expect(res.body).toBeDefined();
+	expect(res.body).toHaveLength(1);
+});
+test("Get -> 'BASE_URL', should return statusCode 200, and res.body.lentgth === 1", async () => {
+	const res = await request(app).get(BASE_URL).set('Authorization', `Bearer ${token}`);
+
+	expect(res.status).toBe(200);
+	expect(res.body).toBeDefined();
+	expect(res.body).toHaveLength(1);
+});
+test("Get -> 'BASE_URL', should return statusCode 200, and res.body.lentgth === 1", async () => {
+	const res = await request(app).get(BASE_URL).set('Authorization', `Bearer ${token}`);
+
+	expect(res.status).toBe(200);
+	expect(res.body).toBeDefined();
+	expect(res.body).toHaveLength(1);
 });
